3bc19e37634c256c21396bc764606dda4d5d32bf
azurecorner <leyegora@yahoo.fr>
2025-07-10 11:14:12 +0200

    add web api

---
+++ src/OtelReferenceApp/WebApi/ObserbilityExtensions.cs
@@ -0,0 +1,124 @@
+﻿using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Serilog;
+
+namespace WebApi.Extensions
+{
+    public static class ObserbilityExtensions
+    {
+        public static IServiceCollection AddObservability(this IServiceCollection services,
+            string serviceName, string sourceName,
+            IConfiguration configuration)
+        {
+            // create the resource that references the service name passed in
+            var resource = ResourceBuilder.CreateDefault().AddService(serviceName: serviceName, serviceVersion: "1.0")
+                .AddAttributes(
+                        new List<KeyValuePair<string, object>>
+                        {
+                        new  ("service.name", serviceName),
+                        new  ("environment", "dev"),
+                        });
+            Console.WriteLine($"OLTP_ENDPOINT ==> {configuration["OLTP_ENDPOINT"]}");
+            // add the OpenTelemetry services
+            var otelBuilder = services.AddOpenTelemetry();
+
+            otelBuilder
+                // add the metrics providers
+                .WithMetrics(metrics =>
+                {
+                    metrics
+                  .SetResourceBuilder(resource)
+                  .AddRuntimeInstrumentation()
+                  .AddAspNetCoreInstrumentation()
+                  .AddHttpClientInstrumentation()
+                  .AddProcessInstrumentation()
+
+                 .AddPrometheusExporter()
+
+                 .AddOtlpExporter(options =>
+                 {
+                     var oltpEndpoint = "https://collector.ashycoast-13bf4b21.westeurope.azurecontainerapps.io/v1/metrics";
+                     if (!string.IsNullOrEmpty(oltpEndpoint))
+                     {
+                         options.Endpoint = new Uri(oltpEndpoint);
+                         options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.HttpProtobuf;
+                     }
+                     else
+                     {
+                         throw new InvalidOperationException("OLTP_ENDPOINT configuration is missing or empty.");
+                     }
+                 });
+                })
+                // add the tracing providers
+                .WithTracing(tracing =>
+                {
+                    tracing.SetResourceBuilder(resource).AddSource(sourceName)
+                            .AddAspNetCoreInstrumentation()
+
+                            .AddHttpClientInstrumentation(options =>
+                            {
+                                options.RecordException = true;
+                                options.EnrichWithHttpRequestMessage = (activity, request) =>
+                                {
+                                    activity.SetTag("http.method", request.Method.Method);
+                                    activity.SetTag("http.url", request?.RequestUri?.ToString());
+                                };
+                                options.FilterHttpRequestMessage = _ => true;
+                                options.FilterHttpWebRequest = _ => true;
+                            })
+
+                            .AddSqlClientInstrumentation(options =>
+                            {
+                                options.SetDbStatementForText = true;
+                                options.RecordException = true;
+                            })
+                            .AddEntityFrameworkCoreInstrumentation()
+
+                               .AddOtlpExporter(options =>
+                               {
+                                   var oltpEndpoint = configuration["OLTP_ENDPOINT"];
+                                   if (!string.IsNullOrEmpty(oltpEndpoint))
+                                   {
+                                       options.Endpoint = new Uri("https://collector.ashycoast-13bf4b21.westeurope.azurecontainerapps.io/v1/traces");
+                                       options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.HttpProtobuf;
+                                   }
+                                   else
+                                   {
+                                       throw new InvalidOperationException("OLTP_ENDPOINT configuration is missing or empty.");
+                                   }
+                               })
+
+                              ;
+                });
+
+            return services;
+        }
+
+        public static void MapObservability(this IEndpointRouteBuilder routes)
+        {
+            Console.WriteLine("==> MapPrometheusScrapingEndpoint");
+            routes.MapPrometheusScrapingEndpoint();
+        }
+
+        public static void AddSerilog(this WebApplicationBuilder builder, string serviceName)
+        {
+            builder.Host.UseSerilog((hostingContext, loggerConfiguration) =>
+               loggerConfiguration
+                   .ReadFrom.Configuration(hostingContext.Configuration)
+                   .Enrich.FromLogContext()
+                   .WriteTo.Console()
+
+                    .WriteTo.OpenTelemetry(options =>
+                    {
+                        options.Endpoint = "https://collector.ashycoast-13bf4b21.westeurope.azurecontainerapps.io/v1/logs";
+                        options.Protocol = Serilog.Sinks.OpenTelemetry.OtlpProtocol.HttpProtobuf;
+
+                        options.ResourceAttributes = new Dictionary<string, object>
+                        {
+                            ["service.name"] = serviceName
+                        };
+                    }));
+        }
+    }
+}
--- src/OtelReferenceApp/WebApi/Program.cs
+++ src/OtelReferenceApp/WebApi/Program.cs
@@ -1,9 +1,20 @@
 using Microsoft.OpenApi.Models;
+using OpenTelemetry.Trace;
+using WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var serviceName = "WeatherForecast.WebApi";
+string sourceName = "WebApi";
+builder.Services.AddObservability(serviceName, sourceName, builder.Configuration);
+// Register the metrics service.
+
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton(TracerProvider.Default.GetTracer(serviceName));
+
+builder.AddSerilog(serviceName);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddSwaggerGen(opts =>
@@ -24,5 +35,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.MapObservability();
 app.Run();
