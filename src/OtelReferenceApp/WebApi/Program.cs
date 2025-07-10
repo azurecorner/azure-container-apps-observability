b68c427fe12fbf953b840109062138ca8f1a9158
azurecorner <leyegora@yahoo.fr>
2025-07-10 08:39:11 +0200

    add web api

---
+++ src/OtelReferenceApp/WebApi/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddSwaggerGen(opts =>
+{
+    opts.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Title = "LogisticManagement API",
+        Version = "v1"
+    });
+});
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
